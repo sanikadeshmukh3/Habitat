0875c88788043f3e917037c657dd41eeb001a1de
sanikadeshmukh3 <sanika.deshmukh135@gmail.com>
2026-03-26 22:54:57 -0400

    succesful connection habit backend to frontend in dashboard

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -3,7 +3,9 @@ const { getDashboardData } = require("../services/dashboardService");
 exports.getDashboard = async (req, res) => {
     try {
         const { userId } = req.params;
+        console.log("User ID received:", userId);
         const data = await getDashboardData(userId);
+        console.log("Data", data);
         res.json(data);
     } catch (error) {
         console.error(error);
