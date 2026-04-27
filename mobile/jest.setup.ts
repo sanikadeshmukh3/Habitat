8a89e586036a60b6082ee7ebf2ec727eda596758
Swetha Mohandass <swethavanith@gmail.com>
2026-04-27 16:24:29 -0400

    added testing for wrapped screen and modified testing for calendar

--- mobile/jest.setup.ts
+++ mobile/jest.setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-native/extend-expect';
+
+jest.mock('@expo/vector-icons', () => ({
+  Ionicons: 'Ionicons',
+  MaterialIcons: 'MaterialIcons',
+  FontAwesome: 'FontAwesome',
+  Feather: 'Feather',
+  AntDesign: 'AntDesign',
+}));
